27ff75e0971226759d01b385394f070b05c420e4
nubmir <rizkysamp2001@gmail.com>
2025-05-07 15:33:17 +0700

    xb

---
+++ app.ts
@@ -0,0 +1,13 @@
+import express, {Request, Response, NextFunction} from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send('Seems Good');
+});
+
+app.listen(3001, () => {
+  console.log('server up!');
+});
+
+export default app;
